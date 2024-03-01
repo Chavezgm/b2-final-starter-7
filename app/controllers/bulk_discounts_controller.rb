8f4a9e847cd6406b29178994cbb44df865d8cbaa
Martin Chavez <martinchavez1203@gmail.com>
2024-02-29 20:56:19 -0700

    Add validations to bulk discount and tested for them

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -6,6 +6,28 @@ class BulkDiscountsController < ApplicationController
   end
 
   def show 
-    
+  end
+
+  def new 
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create 
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = @merchant.bulk_discounts.new(bulk_discount_params)
+
+    if @bulk_discount.save
+      redirect_to merchant_bulk_discounts_path(@merchant)
+      flash[:alert] = 'Bulk discount created'
+    else
+      redirect_to new_merchant_bulk_discount_path(@merchant) #???
+    end
+
+  end
+
+  private 
+
+  def bulk_discount_params
+    params.permit(:percentage_discount, :quantity_threshold)
   end
 end
--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,3 +1,7 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant
+
+  validates :percentage_discount, presence: true
+  validates :quantity_threshold, presence: true 
+
 end
