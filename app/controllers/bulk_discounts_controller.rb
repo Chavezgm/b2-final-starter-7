8647fdd7bf2f977246416a2f0ca9d16d982aa504
Martin Chavez <martinchavez1203@gmail.com>
2024-03-01 00:50:03 -0700

    Add Update action to bulk_discounts controller, US 5 Complete

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -9,9 +9,23 @@ class BulkDiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @bulk_discount = @merchant.bulk_discounts.find(params[:id])
   end
-
+  
   def new 
     @merchant = Merchant.find(params[:merchant_id])
+  end
+  
+  def edit
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = @merchant.bulk_discounts.find(params[:id])
+  end
+
+  def update 
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = @merchant.bulk_discounts.find(params[:id])
+    @bulk_discount.update(bulk_discount_params)
+
+    redirect_to merchant_bulk_discount_path(@merchant, @bulk_discount)
+
   end
 
   def create 
